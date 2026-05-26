2002e82cecaab5115011ad8e99c9110511f5b4c9
Hunter Yarmy <huntery5@nycstudents.net>
2026-05-26 08:45:18 -0400

    Started essential prime table

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,156 @@
-package PACKAGE_NAME;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.io.File;
 
 public class Main {
+
+    private static int numInputs;
+    private static int numOutputs;
+    private static int numTerms;
+    private static Term[] terms;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        read();
+
+        for (int i = 0; i < numOutputs; i++) {
+            ArrayList<Term> unorderedMinterms = new ArrayList<>();
+            ArrayList<Term> realMinterms = new ArrayList<>();
+            HashMap<String, int[]> primeImplicants = new HashMap<>();
+            HashMap<Integer, ArrayList<Term>> minterms = new HashMap<>();
+            for (Term term : terms) {
+                if (term.getOutput()[0].equals("1")) {
+                    unorderedMinterms.add(term);
+                    realMinterms.add(term);
+                }
+                if (term.getOutput()[0].equals("x"))
+                    unorderedMinterms.add(term);
+            }
+
+            for (int j = 0; j <= numInputs; j++) {
+                if (!minterms.containsKey(j)) {
+                    minterms.put(j, new ArrayList<>());
+                }
+                for (Term term : unorderedMinterms) {
+
+                    int count = 0;
+                    for (int k = 0; k < term.getBinaryRep().length(); k++) {
+                        if (term.getBinaryRep().charAt(k) == '1') count++;
+                    }
+
+                    if (count == j) minterms.get(j).add(term);
+                }
+            }
+
+            boolean iterate = true;
+            while (iterate) {
+                HashMap<Integer, ArrayList<Term>> newMinterms = new HashMap<>();
+                for (int j = 0; j < minterms.size() - 1; j++) {
+                    if (!newMinterms.containsKey(j)) {
+                        newMinterms.put(j, new ArrayList<>());
+                    }
+
+                    for (int k = 0; k < minterms.get(j).size(); k++) {
+                        Term curTerm = minterms.get(j).get(k);
+                        for (int l = 0; l < minterms.get(j + 1).size(); l++) {
+                            Term compTerm = minterms.get(j + 1).get(l);
+                            if (Term.onesAligned(curTerm, compTerm) && Term.dashesAligned(curTerm, compTerm)) {
+                                int index = Term.misalignedOnesIndex(curTerm, compTerm);
+                                String newBinRep = curTerm.getBinaryRep().substring(0, index) + "-" + curTerm.getBinaryRep().substring(index + 1);
+                                int[] addOgTerms = mergeIntArray(curTerm.getOriginalTerms(), compTerm.getOriginalTerms());
+
+                                Term addTerm = new Term(newBinRep, curTerm.getOutput(), addOgTerms);
+
+                                newMinterms.get(j).add(addTerm);
+                                curTerm.updateUsed(true);
+                                compTerm.updateUsed(true);
+                            }
+                        }
+                    }
+
+                }
+
+                iterate = false;
+                for (ArrayList<Term> termList : minterms.values()) {
+                    for (Term term : termList) {
+                        if (!term.getUsed()) {
+                            primeImplicants.put(term.getBinaryRep(), term.getOriginalTerms());
+                        }
+                        if (term.getUsed()) iterate = true;
+                    }
+                }
+
+                minterms = newMinterms;
+            }
+            primeImplicants.forEach((key, list) -> System.out.println(key + " : " + Arrays.toString(list)));
+            System.out.println("-----------------------------------------------------");
+
+            //next step is here, you need to do ess. prime implicants
+            int[][] essPrimeTable = new int[realMinterms.size()][primeImplicants.size()];
+
+            for (int[] list : primeImplicants.values()) {
+                for (int k = 0; k < list.length; k++) {
+                    if (list[k] = )
+                }
+            }
+
+        }
+    }
+
+    public static void read() throws FileNotFoundException{
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the amount of inputs: ");
+
+        numInputs = s.nextInt();
+        numTerms = (int) Math.pow(2, numInputs);
+
+        File f = new File("src/WikiTruthTest.csv");
+        s = new Scanner(f);
+
+        terms = new Term[numTerms];
+
+        //get to numbers
+        s.nextLine();
+        s.nextLine();
+
+        //iterate through the whole file
+        int j = 0;
+        while(s.hasNext()) {
+            String line = s.nextLine();
+
+            String[] splitLine = line.split(",");
+
+            numOutputs = splitLine.length - numInputs;
+            String[] outputLine = new String[numOutputs];
+
+            String binaryRep = "";
+
+            for (int i = 0; i < splitLine.length; i++) {
+                if (i < numInputs) {
+                    binaryRep += splitLine[i];
+                } else {
+                    outputLine[i - numInputs] = splitLine[i];
+                }
+            }
+
+            terms[j] = new Term(binaryRep, outputLine);
+            j++;
+        }
+    }
+
+    public static int[] mergeIntArray(int[] arr1, int[] arr2) {
+        int[] newArr = new int[arr1.length + arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            newArr[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            newArr[i + arr2.length] = arr2[i];
+        }
+
+        return newArr;
+    }
 }
--- src/Term.java
+++ src/Term.java
@@ -1,8 +1,99 @@
-public class Minterm {
-    private String binaryRep;
-    private String outputs;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
-    public Minterm(String binaryRep) {
+public class Term {
+    private String binaryRep;
+    private String[] output;
+    private int[] originalTerms;
+    private boolean used;
+
+    public Term(String binaryRep, String[] output) {
         this.binaryRep = binaryRep;
+        this.output = output;
+        originalTerms = new int[]{Integer.parseInt(this.binaryRep, 2)};
+        used = false;
+    }
+
+    public Term(String binaryRep, String[] output, int[] originalTerms) {
+        this.binaryRep = binaryRep;
+        this.output = output;
+        this.originalTerms = originalTerms;
+        used = false;
+    }
+
+    public String getBinaryRep() {
+        return binaryRep;
+    }
+
+    public String[] getOutput() {
+        return output;
+    }
+    public int[] getOriginalTerms() {
+        return originalTerms;
+    }
+
+    public boolean getUsed() {
+        return used;
+    }
+
+    public void updateUsed(boolean value) {
+        used = value;
+    }
+
+    public static boolean onesAligned(Term term1, Term term2) {
+        int count = 0;
+
+        for (int i = 0; i < term1.getBinaryRep().length(); i++) {
+
+            char term2Digit = term2.getBinaryRep().charAt(i);
+            char term1Digit = term1.getBinaryRep().charAt(i);
+
+            if ((term2Digit == '1' && term1Digit == '0') || (term2Digit == '0' && term1Digit == '1')) {
+                count++;
+            }
+        }
+
+        return !(count > 1);
+    }
+
+    public static boolean dashesAligned(Term term1, Term term2) {
+
+        for (int i = 0; i < term1.getBinaryRep().length(); i++) {
+
+            char term2Digit = term2.getBinaryRep().charAt(i);
+            char term1Digit = term1.getBinaryRep().charAt(i);
+
+            if ((term2Digit == '-' && term1Digit != '-') || (term2Digit != '-' && term1Digit == '-')) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     *
+     * @param term1 Term object with same binaryRep length as term2
+     * @param term2 Term object with same binaryRep length as term1
+     * @return the index of two Term objects which have misaligned ones in binaryRep, returns -1 if not found
+     */
+    public static int misalignedOnesIndex(Term term1, Term term2) {
+        for (int i = 0; i < term1.getBinaryRep().length(); i++) {
+
+            char term2Digit = term2.getBinaryRep().charAt(i);
+            char term1Digit = term1.getBinaryRep().charAt(i);
+
+            if ((term2Digit == '1' && term1Digit == '0') || (term2Digit == '0' && term1Digit == '1')) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+       return binaryRep + Arrays.toString(originalTerms);
     }
 }
